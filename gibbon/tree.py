90561a79ab6ca74c78f3d62046f860cf78e62da0
Dawson Booth <github@dawsonbooth.com>
2021-03-09 13:00:18 -0600

    Generalize Tree class

---
+++ gibbon/tree.py
@@ -0,0 +1,96 @@
+from __future__ import annotations
+
+import os
+import shutil
+from concurrent.futures import ProcessPoolExecutor
+from pathlib import Path
+from typing import Callable, Dict, Generic, List, Tuple, Union
+
+from tqdm import tqdm
+
+from gibbon.types import T
+
+
+class Tree(Generic[T]):
+    root: Path
+    glob: str
+    # parse: Callable[[Path], T]  # FIXME: https://github.com/python/mypy/issues/708
+    show_progress: bool
+
+    sources: Tuple[Path, ...]
+    destinations: List[Path]
+    transformations: Dict[str, Callable[[Union[T, Path]], Path]]  # FIXME: https://github.com/python/mypy/issues/3737
+    should_resolve: bool
+
+    def __init__(
+        self,
+        root_folder: Union[str, os.PathLike[str]],
+        glob: str = "**/*.slp",
+        parse: Callable[[Path], Union[T, Path]] = Path,  # FIXME: https://github.com/python/mypy/issues/3737
+        show_progress: bool = False,
+    ):
+        self.root = Path(root_folder)
+        self.glob = glob
+        self.parse = parse
+        self.show_progress = show_progress
+        self.reset()
+
+    def reset(self) -> Tree:
+        self.sources = tuple(self.root.glob(self.glob))
+        self.destinations = list(self.sources)
+        self.transformations = dict()
+
+        return self
+
+    def resolve(self) -> Tree:
+        with ProcessPoolExecutor() as executor:
+            parsed_sources = (p for p in executor.map(Path, self.sources))
+        destinations = self.destinations
+        if self.show_progress:
+            parsed_sources = tqdm(parsed_sources, desc="Process paths", total=len(self.sources))
+            destinations = tqdm(destinations, desc="Move paths")
+
+        for i, parsed_source in enumerate(parsed_sources):
+            # Perform transformations
+            try:
+                for transform in self.transformations.values():
+                    self.destinations[i] = transform(parsed_source)
+            except Exception as e:
+                self.destinations[i] = self.root / e.__class__.__name__ / self.destinations[i].name
+
+        for i, destination in enumerate(destinations):
+            # Rename if duplicate
+            num_duplicates = 0
+            new_name = destination.name
+            while True:
+                renamed = False
+                for j, other in enumerate(self.destinations):
+                    if new_name == other.name and i != j:
+                        num_duplicates += 1
+                        renamed = True
+                        new_name = f"{destination.stem} ({num_duplicates}){destination.suffix}"
+
+                if not renamed:
+                    self.destinations[i] = destination.parent / new_name
+                    break
+
+            # Move file
+            os.makedirs(destination.parent, exist_ok=True)
+            shutil.move(str(self.sources[i]), str(self.destinations[i]))
+
+        # Remove empty folders
+        for path in self.root.rglob("*"):
+            if path.is_dir() and len([p for p in path.rglob("*") if not p.is_dir()]) == 0:
+                if path.exists():
+                    shutil.rmtree(path)
+
+        self.reset()
+
+        return self
+
+    def __enter__(self) -> Tree[T]:
+        return self
+
+    def __exit__(self, *args) -> None:
+        if None in args:
+            self.resolve()
