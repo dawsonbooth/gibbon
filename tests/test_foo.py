02d5caa07b2938efe43fa8c4388894a712ddcff4
Dawson Booth <github@dawsonbooth.com>
2021-03-09 14:13:47 -0600

    Add test suite

--- tests/test_foo.py
+++
@@ -1,5 +0,0 @@
-from gibbon import repeat_string
-
-
-def test_repeat_string():
-    assert repeat_string("abc", 3) == "abcabcabc"
---
+++ tests/test_operations.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+
+from gibbon import Tree
+
+root_folder = Path(__file__).parent / "root"
+
+original_hierarchy = tuple(root_folder.rglob("*.*"))
+
+
+def parse(path: Path) -> str:
+    return path.name
+
+
+def organize(name: str) -> Path:
+    stem = name[: name.index(".")]
+    if stem.isalpha():
+        return root_folder / "alpha" / name
+    elif stem.isnumeric():
+        if bool(int(stem) % 2):
+            return root_folder / "numeric" / "odd" / name
+        else:
+            return root_folder / "numeric" / "even" / name
+    else:
+        return root_folder / "ERROR"
+
+
+def flatten(name: str) -> Path:
+    return root_folder / name
+
+
+def restore_hierarchy():
+    with Tree(root_folder, parse=parse) as tree:
+        tree.transform(flatten)
+
+
+def test_flatten():
+    with Tree(root_folder, parse=parse) as tree:
+        tree.transform(flatten)
+
+    hierarchy = tuple(root_folder.glob("*.*"))
+
+    assert hierarchy == original_hierarchy
+
+    restore_hierarchy()
+
+
+def test_organize():
+    with Tree(root_folder, parse=parse) as tree:
+        tree.transform(organize)
+
+    hierarchy = tuple(root_folder.rglob("*.*"))
+
+    assert len(hierarchy) == len(original_hierarchy)
+    assert hierarchy != original_hierarchy
+
+    restore_hierarchy()
