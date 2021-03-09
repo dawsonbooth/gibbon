90a8bfb15437f956a9322f3038b7a37918a6edf0
Dawson Booth <github@dawsonbooth.com>
2021-03-09 14:14:32 -0600

    Fix top-level exports

--- gibbon/__init__.py
+++ gibbon/__init__.py
@@ -1,23 +1,3 @@
-"""A one line summary of the module or program, terminated by a period.
+from .tree import Tree
 
-Leave one blank line.  The rest of this docstring should contain an
-overall description of the module or program.  Optionally, it may also
-contain a brief description of exported classes and functions and/or usage
-examples.
-
-Typical usage example:
-
-```python
-foo = ClassFoo()
-bar = foo.FunctionBar()
-```
-"""
-
-
-def repeat_string(s: str, n: int) -> str:
-    """Repeat a string an integer number of times!"""
-
-    return s * n
-
-
-__all__ = ["repeat_string"]
+__all__ = ["Tree"]
